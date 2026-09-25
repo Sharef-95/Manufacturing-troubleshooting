5a4545ce8b86b303b5839fa01d5c07950e2c181b
Sharef Elhamamy <sharefalhamamy@gmail.com>
2026-09-24 20:54:27 -0400

    Update

--- Rigid1.py
+++ Rigid1.py
@@ -10,15 +10,16 @@ QA_DATA = {
     "Vacuum Issues": "Check vacuum level by adjusting vacuum knob located behind the machine."
     "if vacuum knob doesn't work, call maintenance.",
     "Blurry Barcodes": "Increase vacuum level, by turning vacuum knob clockwise",
-    "Banding": "step1: wet wipe printheads.  "
-      "step 2: short purge all colours."
-        "step 3: dry wipe"
+    "Banding": "step 1: wet wipe all printheads\n\n"
+"step 2: short purge all printheads.\n\n"
+"step 3: Dry wipe all printheads.\n\n"
+"if banding is still present after wiping, call your supervisor"
     "step 4: print nozzle test."
     "if you still have banding, call maintenance.",
     "Computer Frozen": "Restart DURST program.",
     "Drying Issues": "call maintenance.",
-    "Initialization Issues": "step 1: go printer tab "
-    "step 2:click initialize Sledge"
+    "Initialization Issues": "step 1: go printer tab \n\n"
+    "step 2:click initialize Sledge\n\n"
     "step 3 click initilize printer"
     "if it's still not initilizing, restart the machine.",
     "Missing Nozzles": "step 1: wet wipe printheads"
@@ -28,7 +29,7 @@ QA_DATA = {
     "Lois Sensor Issue": "call maintenance.",
     "Purge Tub Error": "open purge tub, re-close it gently.",
     "Registration Issues": "Check vacuum level.",
-    "UV Issues": "on printer tab, turn UV lamp off, and turn back on."
+    "UV Issues": "on printer tab, turn UV lamp off, and turn back on.\n\n"
     "if UV lamp is still not working, restart the machine.",
     "Communication Issues": "restart the machine.",
     "Head Crash": "on printer tab. Click initilize printer.",
@@ -37,12 +38,16 @@ QA_DATA = {
     "Sledge Control Error": "on printer tab. Click initilize sledge.",
     "Feeding Unit Error": "Check feeding switch is on. The sitch is located at the bottom-left of the front end of the machine.",
     "Computer Frozen": "Restart DURST program.",
-    "Feeder Wheels Not Coming Down": "Check feeding switch is on. The sitch is located at the bottom-left of the front end of the machine.",
+    "Feeder Wheels Not Coming Down": "Check feeding switch is on\n\n."
+    " The switch is located at the bottom-left of the front end of the machine.",
     "Not Recognizing Purge Tub Is Open for Purge": "open purge tub, re-close it gently.",
-    "UV Lamp Error After Sledge Crash": "on printer tab, turn UV lamp off, and turn back on."
-    "if UV lamp is still not working, restart the machine.",
+    "UV Lamp Error After Sledge Crash": "on printer tab, turn UV lamp off, and turn it back on.\n\n"
+"if it doesn't work restart the machine\n\n"
+"if it still not worling after restarting the machine, call maintenance.",
     "Power Trip Off": "Restart the machine",
-    "ink spray":"short purge all printheads, and dry wipe."
+    "ink spray":"short purge all printheads, and dry wipe.",
+    "Error In Checking Printhead Ink Tanks":"Left up ink waste lever, and close down gently.\n\n"
+"if error isn't cleared, call maintenance."
    
 }
 
